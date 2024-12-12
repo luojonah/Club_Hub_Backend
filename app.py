e54f7b51f3e7ccc2d60d3b2503c5b2ae2e7e7b19
luojonah <jonahluo22@gmail.com>
2024-12-12 10:01:04 -0800

    added my info to app.py

--- app.py
+++ app.py
@@ -39,6 +39,27 @@ def get_ansh():
     }
     return jsonify(ansh_data)
 
+# API endpoint to return general data
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = [
+        {"FirstName": "Jonah", "LastName": "Luo", "Age": 15, "Email": "jonahluo22@gmail.com"},
+    ]
+    return jsonify(data)
+
+# API endpoint specific to Gyutae
+@app.route('/api/Jonah', methods=['GET'])
+def get_gyutae():
+    gyutae_data = {
+        "FirstName": "Jonah",
+        "LastName": "Luo",
+        "Age": 15,
+        "Email": "jonahluo22@gmail.com",
+        "Hobbies": ["Soccer", "Movies", "Hiking", "Camping"],
+        "Residence": "San Diego"
+    }
+    return jsonify(gyutae_data)
+
 # Root endpoint to return a simple HTML page
 @app.route('/')
 def home():
